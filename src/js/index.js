eebe0fa01661382a822bee1e76ffacc0d05c5018
Вильгельм <vito.fincher@list.ru>
2024-02-26 21:58:32 +1000

    test

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,4 @@
 import mobileNav from './modules/mobile-nav.js';
+import checkBlocksVisibility from './modules/scroll-app.js';
 mobileNav();
+checkBlocksVisibility();
---
+++ src/js/modules/scroll-app.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let blocks = document.querySelectorAll('.items-process__item');
+ 
+    function checkBlocksVisibility() {
+        let windowHeight = window.innerHeight;
+ 
+        blocks.forEach(block => {
+            let blockPosition = block.getBoundingClientRect().top;
+ 
+            if (blockPosition < windowHeight - 100) {
+                block.style.opacity = "1";
+                block.style.transform = "translateY(0)";
+            }
+        });
+    }
+ 
+    checkBlocksVisibility();
+ 
+    window.addEventListener('scroll', function() {
+        checkBlocksVisibility();
+    });
+});
